5e6f93732ec911f3f43ab8de7f52ac5f8f5905f4
Tommy Berman <tomy.berman1@gmail.com>
2019-12-06 22:55:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,10 @@ def roll_call_dwarves(dwarves)
 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+
+calls.map {|element| 
+
 end
 
 def long_planeteer_calls# code an argument here
